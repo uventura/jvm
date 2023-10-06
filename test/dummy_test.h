06edb8439c832447f83a3669964784a3a0abd1ff
uventura <ualivent@gmail.com>
2023-10-06 17:49:09 -0300

    Applying Clang Format in All files under the project

--- test/dummy_test.h
+++ test/dummy_test.h
@@ -1,8 +1,8 @@
-#include "unity/unity.h"
 #include "lib/init/dummy.h"
+#include "unity/unity.h"
 
-void testing_unity_behavior(void)
+void
+testing_unity_behavior (void)
 {
-  TEST_ASSERT_EQUAL(sum(1,0), 1);
+  TEST_ASSERT_EQUAL (sum (1, 0), 1);
 }
-
