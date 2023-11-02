a73fe62441bfe99b414c61722ba025c3ccbdb4d9
uventura <ualivent@gmail.com>
2023-11-02 19:02:49 -0300

    Defining Basic Types

---
+++ src/lib/base/defines.h
@@ -0,0 +1,11 @@
+#ifndef JVM_DEFINES
+#define JVM_DEFINES
+
+#include <stdint.h>
+
+typedef unsigned char u1;
+typedef unsigned short u2;
+typedef unsigned int u4;
+typedef unsigned long u8;
+
+#endif
