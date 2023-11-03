57634b7bff5bcd14686024cca636d67e8139da2c
uventura <ualivent@gmail.com>
2023-11-03 14:47:42 -0300

    Testing Constant Pool Value

--- test/main.c
+++ test/main.c
@@ -18,6 +18,6 @@ int main()
     RUN_TEST(testing_u2_read_cafe);
     RUN_TEST(testing_u2_read_babe);
     RUN_TEST(testing_u4_read_cafe_babe);
-    RUN_TEST(test_bootrstrap_get_cafe_babe);
+    RUN_TEST(test_bootstrap_get_cafe_babe);
     return UNITY_END();
 }
--- test/test_bootstrap.h
+++ test/test_bootstrap.h
@@ -1,10 +1,21 @@
 #include "unity/unity.h"
+
 #include <stdio.h>
+#include <string.h>
 
 #include "lib/class_loader/bootstrap/bootstrap.h"
 
-void test_bootrstrap_get_cafe_babe(void)
+void test_bootstrap_get_cafe_babe(void)
 {
     ClassFile class_file = load_class_file("data/examples/HelloWorld.class");
     TEST_ASSERT_EQUAL(class_file.magic, 0XCAFEBABE);
+}
+
+void test_bootstrap_get_utf8_field(void)
+{
+    ClassFile class_file = load_class_file("data/examples/HelloWorld.class");
+    char element[100];
+    memcpy(element, class_file.constant_pool[11].info.Utf8.bytes, class_file.constant_pool[11].info.Utf8.length);
+    element[class_file.constant_pool[11].info.Utf8.length] = '\0';
+    TEST_ASSERT_EQUAL(0, strcmp(element, "([Ljava/lang/String;)V"));
 }
