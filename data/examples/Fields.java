e408958794870c6fa4fa6dfb96c2a546fc512a8f
uventura <ualivent@gmail.com>
2023-11-04 12:10:27 -0300

    Field Example

---
+++ data/examples/Fields.java
@@ -0,0 +1,7 @@
+public class Fields {
+  public static int random_field;
+  public static void main(String[] args) {
+    System.out.println("Example with Fields");
+    System.out.println(random_field);
+  }
+}
