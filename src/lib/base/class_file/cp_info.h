0f54ded64805adf7ef88368a6de671eb895379eb
uventura <ualivent@gmail.com>
2023-11-04 11:59:00 -0300

    Removing Defines from cp_info

--- src/lib/base/class_file/cp_info.h
+++ src/lib/base/class_file/cp_info.h
@@ -1,21 +1,10 @@
 #ifndef JVM_CP_INFO
 #define JVM_CP_INFO
 
-#include "lib/base/defines.h"
-
 // Reference: https://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html#jvms-4.4
 
-#define CONSTANT_Class 7
-#define CONSTANT_Fieldref 9
-#define CONSTANT_Methodref 10
-#define CONSTANT_InterfaceMethodref 11
-#define CONSTANT_String 8
-#define CONSTANT_Integer 3
-#define CONSTANT_Float 4
-#define CONSTANT_Long 5
-#define CONSTANT_Double 6
-#define CONSTANT_NameAndType 12
-#define CONSTANT_Utf8 1
+#include "lib/base/class_file/class_defines.h"
+#include "lib/base/defines.h"
 
 typedef struct
 {
