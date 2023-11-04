51f1b8d512f2f7b4a61b22a252e808bbded72c34
uventura <ualivent@gmail.com>
2023-11-04 12:44:18 -0300

    Adding another field in Field Example

--- data/examples/Fields.java
+++ data/examples/Fields.java
@@ -1,5 +1,6 @@
 public class Fields {
   public static int random_field;
+  public static int another_field = 42;
   public static void main(String[] args) {
     System.out.println("Example with Fields");
     System.out.println(random_field);
