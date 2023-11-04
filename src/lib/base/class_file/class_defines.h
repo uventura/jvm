8688398563c95030af71d20661befb2d9b45de69
uventura <ualivent@gmail.com>
2023-11-04 11:57:56 -0300

    Adding Class Defines

---
+++ src/lib/base/class_file/class_defines.h
@@ -0,0 +1,25 @@
+#ifndef JVM_CLASS_DEFINES
+#define JVM_CLASS_DEFINES
+
+// Constant Pool Tags
+#define CONSTANT_Class 7
+#define CONSTANT_Fieldref 9
+#define CONSTANT_Methodref 10
+#define CONSTANT_InterfaceMethodref 11
+#define CONSTANT_String 8
+#define CONSTANT_Integer 3
+#define CONSTANT_Float 4
+#define CONSTANT_Long 5
+#define CONSTANT_Double 6
+#define CONSTANT_NameAndType 12
+#define CONSTANT_Utf8 1
+
+// Attributes Info
+typedef struct
+{
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u1 *info;
+} attribute_info;
+
+#endif
