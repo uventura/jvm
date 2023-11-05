96dcb38c75b06cbbd56e75b27be813d8a327e3c0
uventura <ualivent@gmail.com>
2023-11-05 12:12:45 -0300

    Fixing Example Command

--- src/main.c
+++ src/main.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char **argv)
 {
-    // Command Example: ./bin/jvm -read data/example/Fields.class
+    // Command Example: ./bin/jvm -read data/examples/Fields.class
     jvm_run(argc, argv);
 }
