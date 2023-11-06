7c42ca9fc5b55eda5c180002f712371d87472c2a
João Aragão <joao.sales.aragao@gmail.com>
2023-11-06 16:44:56 -0300

    testing general informations of soma .class

--- test/main.c
+++ test/main.c
@@ -19,5 +19,13 @@ int main()
     RUN_TEST(testing_u2_read_babe);
     RUN_TEST(testing_u4_read_cafe_babe);
     RUN_TEST(test_bootstrap_get_cafe_babe);
+    //TESTING GENERAL INFORMATIONS OF .CLASS SOMA (interface)
+    RUN_TEST(test_bootstrap_get_cafe_babe_soma);
+    RUN_TEST(test_bootstrap_constant_pool_count_soma);
+    RUN_TEST(test_bootstrap_access_flags_soma);
+    RUN_TEST(test_bootstrap_interfaces_count_soma);
+    RUN_TEST(test_bootstrap_fields_count_soma);
+    RUN_TEST(test_bootstrap_methods_count_soma);
+    RUN_TEST(test_bootstrap_attributes_count_soma);
     return UNITY_END();
 }
