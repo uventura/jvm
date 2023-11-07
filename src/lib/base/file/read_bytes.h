255492194d6ae58c71da8a4ca2275108ea631fa9
Victor <89362863+VictorFernandesUnB@users.noreply.github.com>
2023-11-07 10:56:54 -0300

    Update read_bytes.h comments

--- src/lib/base/file/read_bytes.h
+++ src/lib/base/file/read_bytes.h
@@ -4,6 +4,8 @@
 #include "lib/base/defines.h"
 #include <stdio.h>
 
+// Header with the prototypes for the functions utilized to read different integer types.
+
 u1 u1_read(FILE *file);
 u2 u2_read(FILE *file);
 u4 u4_read(FILE *file);
