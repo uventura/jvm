c9e9424dae8e2daec6d8fe4249e965f7c1892213
Victor <89362863+VictorFernandesUnB@users.noreply.github.com>
2023-11-07 11:01:23 -0300

    Update cp_info.c comments

--- src/lib/base/class_file/cp_info.c
+++ src/lib/base/class_file/cp_info.c
@@ -3,6 +3,7 @@
 
 #include "lib/base/class_file/cp_info.h"
 
+// Function to copy UTF-8 bytes in string.
 void get_utf8_value(u2 index, cp_info *constant_pool, char *buffer)
 {
     memcpy((void *)buffer, constant_pool[index].info.Utf8.bytes, constant_pool[index].info.Utf8.length);
