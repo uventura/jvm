e4f4a523264442ee1519b5fd964f189025633437
uventura <ualivent@gmail.com>
2023-11-08 02:41:26 -0300

    Fields2 Example

---
+++ data/examples/Fields2.java
@@ -0,0 +1,12 @@
+ public class Fields2 {
+
+      private static String str = "Default";
+
+      public String getString() {
+          return str;
+      }
+
+      public void setString(String s) {
+          str = s;
+      }
+  }
