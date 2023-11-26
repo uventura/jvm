328a8cdbae47f1a1fa358c7a344d371c18d929da
uventura <ualivent@gmail.com>
2023-11-26 12:14:54 -0300

    Fixing free

--- src/lib/base/stack/stack.c
+++ src/lib/base/stack/stack.c
@@ -25,14 +25,14 @@ struct Node *stack_create_node(void *data)
 
 void stack_push(Stack *stack, void *data)
 {
-    Node *new_top = create_node(data);
+    Node *new_top = stack_create_node(data);
     new_top->next = stack->top;
     stack->top = new_top;
 }
 
 struct Node *stack_pop(Stack *stack)
 {
-    if (is_empty(stack))
+    if (stack_is_empty(stack))
     {
         fprintf(stderr, "ERROR: Attempted to pop from an empty stack\n");
         exit(EXIT_FAILURE);
@@ -47,7 +47,7 @@ struct Node *stack_pop(Stack *stack)
 
 struct Node *stack_top(Stack *stack)
 {
-    if (is_empty(stack))
+    if (stack_is_empty(stack))
     {
         fprintf(stderr, "ERROR: Attempted to access top element of an empty stack\n");
         exit(EXIT_FAILURE);
@@ -60,8 +60,11 @@ struct Node *stack_top(Stack *stack)
 
 void free_stack(Stack *stack)
 {
-    while (!is_empty(stack))
+    struct Node *current_node;
+    while (!stack_is_empty(stack))
     {
-        pop(stack);
+        current_node = (stack->top)->next;
+        free(stack->top);
+        stack->top = current_node;
     }
 }
