e06ae79c744d283adaa591fafba624eb7c62e04f
uventura <ualivent@gmail.com>
2023-11-26 12:14:54 -0300

    Fixing free

--- src/lib/base/stack/stack.h
+++ src/lib/base/stack/stack.h
@@ -17,6 +17,6 @@ struct Node *stack_create_node(void *data);
 void stack_push(Stack *stack, void *data);
 struct Node *stack_pop(Stack *stack);
 struct Node *stack_top(Stack *stack);
-void free_stack(Stack *stack)
+void free_stack(Stack *stack);
 
 #endif
