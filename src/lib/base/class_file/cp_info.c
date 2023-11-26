58db88c40c624c109c430774f1906b73b537a6cd
uventura <ualivent@gmail.com>
2023-11-26 17:37:57 -0300

    Adding Super Class Reading in JVM Reader

--- src/lib/base/class_file/cp_info.c
+++ src/lib/base/class_file/cp_info.c
@@ -9,3 +9,9 @@ void get_utf8_value(u2 index, cp_info *constant_pool, char *buffer)
     memcpy((void *)buffer, constant_pool[index].info.Utf8.bytes, constant_pool[index].info.Utf8.length);
     buffer[constant_pool[index].info.Utf8.length] = '\0';
 }
+
+void get_class_name(u2 class_index, cp_info *constant_pool, char *buffer)
+{
+    u2 this_class_index = constant_pool[class_index - 1].info.Class.name_index;
+    get_utf8_value(this_class_index - 1, constant_pool, buffer);
+}
