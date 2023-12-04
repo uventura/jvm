962e4e3a5ddb230990067e7d4228c35c13a4cee7
uventura <ualivent@gmail.com>
2023-12-04 16:09:33 -0300

    Adding descriptor type

---
+++ src/lib/interpreter/method_descriptors.h
@@ -0,0 +1,15 @@
+#ifndef JVM_METHOD_DESCRIPTORS
+#define JVM_METHOD_DESCRIPTORS
+
+#define VOID_TYPE 'V'
+#define INT_TYPE 'I'
+#define LONG_TYPE 'J'
+#define FLOAT_TYPE 'F'
+#define DOUBLE_TYPE 'D'
+#define BYTE_TYPE 'B'
+#define SHORT_TYPE 'S'
+#define CHAR_TYPE 'C'
+#define BOOLEAD_TYPE 'Z'
+#define CLASS_TYPE 'L'
+
+#endif
