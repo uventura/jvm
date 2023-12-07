29995c86f3675f3ce85dc174745eb5a1bcdcf42b
uventura <ualivent@gmail.com>
2023-12-07 19:30:53 -0300

    Loading Float parameters

--- data/examples/ObjectExample.java
+++ data/examples/ObjectExample.java
@@ -1,12 +1,15 @@
 public class ObjectExample {
     int x;
-    public ObjectExample(int val_x)
+    float y;
+    public ObjectExample(int val_x, float val_y)
     {
         this.x = val_x;
+        this.y = val_y;
     }
 
     public static void main(String[] args) {
-    ObjectExample myObj = new ObjectExample(8);
+    ObjectExample myObj = new ObjectExample(8, 2.4f);
     System.out.println(myObj.x);
+    System.out.println(myObj.y);
     }
 }
--- src/lib/runtime_data_area/object.c
+++ src/lib/runtime_data_area/object.c
@@ -7,12 +7,12 @@ JVMObject *object_init_dynamic(ClassFile *ref_class)
 {
     JVMObject *object = (JVMObject *)malloc(sizeof(JVMObject));
     object->class = ref_class;
-    object->data = (JVMObjectValue *)malloc(sizeof(JVMObjectValue));
+    object->data = (JVMObjectValue *)malloc(sizeof(JVMObjectValue) * ref_class->fields_count);
 
     u2 index;
     for (index = 0; index < ref_class->fields_count; index++)
     {
-        object->data[index].field = &(ref_class->fields[index]);
+        object->data[index].field = ref_class->fields + index;
     }
 
     return object;
