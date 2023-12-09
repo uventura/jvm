984a8c4fec21a748ecaff2220e3ee02d1354d0be
João Aragão <joao.sales.aragao@gmail.com>
2023-12-09 15:17:44 -0300

    opcodes for sub and mul

---
+++ data/examples/Arithmetic_Operations.java
@@ -0,0 +1,9 @@
+public class Arithmetic_Operations {
+    public static void main(String args[]) {
+      System.out.println(3);
+      int x = 99;
+      int y = 49;
+      //int z = x-y;
+      int z = x*y;
+    }
+}
