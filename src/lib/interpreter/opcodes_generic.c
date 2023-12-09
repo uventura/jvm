b3f8702a184d9d23f932374fea20147b48f6160f
uventura <ualivent@gmail.com>
2023-12-09 20:17:04 -0300

    Adding Opcode 0xA0 implementation

---
+++ src/lib/interpreter/opcodes_generic.c
@@ -0,0 +1,9 @@
+#include "lib/interpreter/opcodes_generic.h"
+
+u2 get_branch_offset(MethodData *method_data)
+{
+    u2 branchbyte1 = method_data->code.code[method_data->pc + 1];
+    u2 branchbyte2 = method_data->code.code[method_data->pc + 2];
+    u2 offset = (branchbyte1 << 8) | branchbyte2;
+    return offset;
+}
