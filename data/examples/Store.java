2a19a9b2b35100e10f07602e72e81c6771edfea0
JOAO VICTOR ALVES DOS SANTOS <hyperlobe2@gmail.com>
2023-12-09 17:32:14 -0300

    Store example and loads

---
+++ data/examples/Store.java
@@ -0,0 +1,20 @@
+public class Store {
+    public static void main(String[] args) {
+        // Inicializando variáveis
+        int num1 = 10;     // Pode gerar um opcode istore
+        int num2 = 20;     // Pode gerar um opcode istore
+        int index = 0;     // Pode gerar um opcode istore_0
+
+        // Inicializando um array de short
+        short[] shortArray = new short[5];
+
+        // Executando uma operação e armazenando o resultado em uma variável
+        int sum = num1 + num2; // Pode gerar um opcode istore
+
+        // Armazenando um valor no array (conversão de int para short)
+        shortArray[index] = (short)sum; // Pode gerar um opcode sastore
+
+        // Exibindo o resultado
+        System.out.println("Valor armazenado no array: " + shortArray[index]);
+    }
+}
