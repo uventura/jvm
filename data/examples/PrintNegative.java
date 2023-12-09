143e36672f3c6d26c74907cd1e4ed68149845859
uventura <ualivent@gmail.com>
2023-12-09 08:00:29 -0300

    Adding example

---
+++ data/examples/PrintNegative.java
@@ -0,0 +1,10 @@
+public class PrintNegative {
+    public static void main (String[ ] s)
+    {
+        int x = -1;
+        System.out.println(x);
+
+        float y = 3.14f;
+        System.out.println(y);
+    }
+}
