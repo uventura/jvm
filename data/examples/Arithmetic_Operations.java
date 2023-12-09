8a6691f0acac9e28c707c6f5d910a3f9d11aebac
João Aragão <joao.sales.aragao@gmail.com>
2023-12-09 17:39:33 -0300

    opcodes for bitwise operations, div and rem.

--- data/examples/Arithmetic_Operations.java
+++ data/examples/Arithmetic_Operations.java
@@ -2,8 +2,17 @@ public class Arithmetic_Operations {
     public static void main(String args[]) {
       System.out.println(3);
       int x = 99;
-      int y = 49;
-      //int z = x-y;
-      int z = x*y;
+      int y = 3;
+      int z = x-y; // tests isub
+      z = x*y; // tests imul
+      z = x/y; // tests idiv
+      z = x % y; // tests irem
+      z = -x; // tests ineg
+      z = x << y;
+      z = x >> y;
+      z = x >>> y;
+      z = x & y;
+      z = x | y;
+      z = x ^ y;
     }
 }
