9bb8b0b98445941612a692f182e24ec28fc48e97
ualitonsilvapartner <ualiton.silva@partner.bmw.com.br>
2023-12-10 10:35:16 -0300

    Fixing Windows print

--- src/lib/environment/jvm_debug.h
+++ src/lib/environment/jvm_debug.h
@@ -1,6 +1,12 @@
 #ifndef JVM_GLOBALS
 #define JVM_GLOBALS
 
+#ifdef _WIN32
+    #define JVM_WINDOWS "windows"
+#else
+    #define JVM_UNIX "unix"
+#endif
+
 #include "lib/environment/jvm_debug_functions.h"
 
 extern u1 JVM_DEBUG_MODE_ACTIVE;
