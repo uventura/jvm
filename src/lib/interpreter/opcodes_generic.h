832a2ae1c7fc32c26db0957ac888e82eaa7e79ba
uventura <ualivent@gmail.com>
2023-12-13 19:12:24 -0300

    Adding dstore generic

--- src/lib/interpreter/opcodes_generic.h
+++ src/lib/interpreter/opcodes_generic.h
@@ -3,9 +3,12 @@
 
 #include "lib/base/defines.h"
 #include "lib/runtime_data_area/method_area.h"
+#include "lib/runtime_data_area/frame.h"
 
 u2 get_branch_offset(MethodData *method_data);
 int get_branch_offset_int(MethodData *method_data);
 int get_branch_offset_int_2(MethodData *method_data);
+
+void dstore_generic(Frame* current_frame, u2 index);
 
 #endif
