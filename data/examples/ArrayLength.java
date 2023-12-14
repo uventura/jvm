2b233cd047152404f4071ae98a666e2b6a9a36b3
uventura <ualivent@gmail.com>
2023-12-13 23:14:00 -0300

    Testing array length

---
+++ data/examples/ArrayLength.java
@@ -0,0 +1,12 @@
+public class ArrayLength {
+    public static void main(String args[]) {
+        int[] vetor = { 5, 9, 7, 4, 6, 1, 2 };
+
+        int i = 0;
+        while(i < vetor.length)
+        {
+            System.out.println(vetor[i]);
+            i+=1;
+        }
+    }
+}
